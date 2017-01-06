3916020d41ebf8f179899b862cb4b5ce1503a6e6
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-01-05 19:25:05 -0500

    Hamburger Animation

--- js/toggleHamburger.js
+++ js/toggleHamburger.js
@@ -1,5 +1,24 @@
-$(document).ready(function () {
-			  $(".navbar-toggle").on("click", function () {
-				    $(this).toggleClass("active");
-			  });
-		});
+// $(document).ready(function () {
+// 			  $(".navbar-toggle").on("click", function () {
+// 				    $(this).toggleClass("active");
+// 			  });
+// 		});
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
